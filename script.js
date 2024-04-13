a87a788102e25cd9f9579fb1ab912f5ef7026e35
salah <laarajsalaheddine@gmail.com>
2024-04-13 16:33:21 +0000

    small edit on js functions

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function editRow(id) {
+const editRow = (id) => {
     // id  <==> la le numéro de ligne selectionnée
     window.location.href = `editer.php?id=${id}`;
-}
-function getSelectedCheckboxs() {
+};
+
+const getSelectedCheckboxs = () => {
     // fontion qui retourne les checkbox selectionnés
-}
-function getNumberOfSelectedLines() {
+};
+
+const getNumberOfSelectedLines = () => {
     // function qui retourne the nombre des lignes selectionnés  
-}
-function isOneLineSelected() {
+};
+
+const isOneLineSelected = () => {
     // cela fait un test si une seul ligne à été selectionnées
-}
+};
